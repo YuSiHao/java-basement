09a1e870cc8485edb8a79ae4035483eb19709633
chandler <1941719093@qq.com>
2017-03-09 17:46:44 +0800

    learning

---
+++ src/com/ysh/java/basement/rotate/Node.java
@@ -0,0 +1,73 @@
+package com.ysh.java.basement.rotate;
+
+//one way linked node list
+public class Node {
+
+	int value;
+	Node nextNode;
+
+	public Node(int value, Node nextNode) {
+		super();
+		this.value = value;
+		this.nextNode = nextNode;
+	}
+
+	public static Node init(int num) {
+		Node node = new Node(0, null);
+		Node cur = null;
+		Node temp = null;
+		for (int i = 1; i < num; i++) {
+			temp = new Node(i, null);
+			if (i == 1) {
+				node.nextNode = temp;
+			} else {
+				cur.nextNode = temp;
+			}
+			cur = temp;
+		}
+		return node;
+	}
+
+	// print node value
+	private static void out(Node head) {
+		Node tempNode = head;
+		while (tempNode != null) {
+			System.err.println(tempNode.value);
+			tempNode = tempNode.nextNode;
+		}
+	}
+
+	/**
+	 * 反转单链表
+	 * 
+	 * @param head
+	 * @return
+	 */
+	private static Node reverseHead(Node head) {
+		if (head == null) {
+			return head;
+		}
+
+		Node pre = head;
+		Node cur = head.nextNode;
+		Node next = null;
+		while (cur != null) {
+			next = cur.nextNode;
+			cur.nextNode = pre;
+
+			pre = cur;
+			cur = next;
+		}
+		head.nextNode = null;
+		head = pre;
+		return head;
+	}
+
+	public static void main(String[] args) {
+		Node head = init(10);
+		out(head);
+		Node reverseHead = reverseHead(head);
+		out(reverseHead);
+	}
+
+}
---
+++ src/com/ysh/java/basement/rotate/RotateString.java
@@ -0,0 +1,10 @@
+package com.ysh.java.basement.rotate;
+
+public class RotateString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
