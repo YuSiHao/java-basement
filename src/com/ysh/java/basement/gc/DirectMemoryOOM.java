c909a406cb2a15ddd2e8b0cdaa580539daae9578
chandler <1941719093@qq.com>
2017-03-20 10:08:08 +0800

    finish

--- src/com/ysh/java/basement/gc/DirectMemoryOOM.java
+++ src/com/ysh/java/basement/gc/DirectMemoryOOM.java
@@ -2,15 +2,20 @@ package com.ysh.java.basement.gc;
 
 import java.lang.reflect.Field;
 
-public class DirectMemoryOOM {
+import sun.misc.Unsafe;
+
+public class DirectMemoryOOM {//-verbose:gc -Xmx20M -XX:MaxDirectMemorySize=10M -XX:-PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError
 	
 	private static final int _1MB = 1024 * 1024;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		/*Field unsafeField = sun.misc.Unsafe.class.getDeclaredField()[0];
+		Field unsafeField = sun.misc.Unsafe.class.getDeclaredFields()[0];
 		unsafeField.setAccessible(true);
-		sun.misc.Unsafe unsafe = unsafeField.get(null);*/
+		sun.misc.Unsafe unsafe = (Unsafe) unsafeField.get(null);
+		while(true){
+			unsafe.allocateMemory(_1MB);
+		}
 	}
 
 }
